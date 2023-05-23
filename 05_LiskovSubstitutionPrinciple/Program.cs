dad5201c8587f63f34c22a53a58a51b1dc942f76
matheus-l-oliveira <matheusluis.dev@gmail.com>
2023-05-22 21:13:35 -0300

    05 & 06 file

---
+++ 05_LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+
+Document doc1 = new("doc1Data", "doc1.txt");
+WritableDocument doc2 = new("doc2Data", "doc2.txt");
+
+Project project = new(new List<Document> { doc1, doc2 }, new List<WritableDocument> { doc2 });
+project.SaveAll();
+project.OpenAll();
+
+class Document
+{
+    public string Data { get; set; }
+    public string FileName { get; set; }
+
+    public Document(string data, string fileName)
+    {
+        Data = data;
+        FileName = fileName;
+    }
+
+    public void Open()
+    {
+        Console.WriteLine($"Opening file: {FileName}...");
+        Console.WriteLine(Data);
+        Console.WriteLine(new string('=', 50));
+    }
+}
+
+class WritableDocument : Document
+{
+    public WritableDocument(string data, string fileName) : base(data, fileName)
+    {
+        Data = data;
+        FileName = fileName;
+    }
+
+    public void Save()
+    {
+        Console.WriteLine($"Saving file... {FileName}");
+        Console.WriteLine(new string('=', 50));
+    }
+}
+
+class Project
+{
+    public List<Document> AllDocs;
+    public List<WritableDocument> WritableDocs;
+
+    public Project(List<Document> allDocs, List<WritableDocument> writableDocs)
+    {
+        AllDocs = allDocs;
+        WritableDocs = writableDocs;
+    }
+
+    public void OpenAll()
+    {
+        foreach (var doc in AllDocs)
+        {
+            doc.Open();
+        }
+    }
+
+    public void SaveAll()
+    {
+        foreach (var writableDoc in WritableDocs)
+        {
+            writableDoc.Save();
+        }
+    }
+}
---
+++ 06_InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,67 @@
+﻿Amazon amazon = new();
+amazon.CreateServer("CreateServer");
+amazon.ListServers("ListServers");
+amazon.GetCDNAddress();
+amazon.StoreFile("Name");
+amazon.GetFile("Name");
+
+Dropbox dropbox = new();
+dropbox.StoreFile("Name");
+dropbox.GetFile("Name");
+
+interface ICloudHostingProvider
+{
+    void CreateServer(string region);
+    void ListServers(string region);
+}
+
+interface ICDNProvider
+{
+    void GetCDNAddress();
+}
+
+interface ICloudStorageProvider
+{
+    void StoreFile(string name);
+    void GetFile(string name);
+}
+
+class Amazon : ICloudHostingProvider, ICDNProvider, ICloudStorageProvider
+{
+    public void CreateServer(string region)
+    {
+        //
+    }
+
+    public void ListServers(string region)
+    {
+        //
+    }
+
+    public void GetCDNAddress()
+    {
+        //
+    }
+
+    public void StoreFile(string name)
+    {
+        //
+    }
+
+    public void GetFile(string name)
+    {
+        //
+    }
+}
+
+class Dropbox : ICloudStorageProvider{
+    public void StoreFile(string name)
+    {
+        //
+    }
+
+    public void GetFile(string name)
+    {
+        //
+    }
+}
